da76b8d1b29eaaae1a71c1e7151ff348c695814f
Мадина <mbudykova@mail.ru>
2025-12-06 17:53:52 +0500

    Task2

--- Tyuiu.BudykovaMM.Sprint6.Task2.V7.Test/DataServiceTest.cs
+++ Tyuiu.BudykovaMM.Sprint6.Task2.V7.Test/DataServiceTest.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tyuiu.BudykovaMM.Sprint6.Task2.V7.Lib;
+using System;
+
 namespace Tyuiu.BudykovaMM.Sprint6.Task2.V7.Test
 {
     [TestClass]
@@ -12,29 +14,36 @@ namespace Tyuiu.BudykovaMM.Sprint6.Task2.V7.Test
 
             int startValue = -5;
             int stopValue = 5;
+
             int len = stopValue - startValue + 1;
 
+            
             double[] valueWaitArray = new double[len];
-            valueWaitArray[0] = -9.10;   
-            valueWaitArray[1] = 1.55;  
-            valueWaitArray[2] = 292.78;  
-            valueWaitArray[3] = -0.57;   
-            valueWaitArray[4] = -0.35;  
-            valueWaitArray[5] = 2.00;  
-            valueWaitArray[6] = 4.35;    
-            valueWaitArray[7] = 4.57;    
-            valueWaitArray[8] = -288.78; 
-            valueWaitArray[9] = 2.45;    
-            valueWaitArray[10] = 13.10;  
+            valueWaitArray[0] = -9.10;   // x = -5
+            valueWaitArray[1] = 1.55;    // x = -4
+            valueWaitArray[2] = 292.78;  // x = -3
+            valueWaitArray[3] = -0.57;   // x = -2
+            valueWaitArray[4] = -0.35;   // x = -1
+            valueWaitArray[5] = 2.00;    // x = 0
+            valueWaitArray[6] = 4.35;    // x = 1
+            valueWaitArray[7] = 4.57;    // x = 2
+            valueWaitArray[8] = -288.78; // x = 3
+            valueWaitArray[9] = 2.45;    // x = 4
+            valueWaitArray[10] = 13.10;  // x = 5
 
+            
             double[] res = ds.GetMassFunction(startValue, stopValue);
 
             
             for (int i = 0; i < len; i++)
             {
-                Assert.AreEqual(valueWaitArray[i], res[i], 0.01);
+                
+                double roundedRes = Math.Round(res[i], 2);
+
+                
+                Assert.AreEqual(valueWaitArray[i], roundedRes, 0.001,
+                    $"Ошибка при x={startValue + i}. Ожидалось: {valueWaitArray[i]}, Получено: {res[i]} (округляется до {roundedRes})");
             }
         }
     }
 }
-
