19f02608b6ed42a444aebea8d4c11d2859f0f564
Мадина <mbudykova@mail.ru>
2025-12-06 18:06:14 +0500

    Task2

--- Tyuiu.BudykovaMM.Sprint6.Task2.V7.Lib/DataService.cs
+++ Tyuiu.BudykovaMM.Sprint6.Task2.V7.Lib/DataService.cs
@@ -6,25 +6,32 @@ namespace Tyuiu.BudykovaMM.Sprint6.Task2.V7.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            double[] valueArray;
-            int len = (stopValue - startValue) + 1;
-            valueArray = new double[len];
-            double y;
+            int len = stopValue - startValue + 1;
+            double[] valueArray = new double[len];
             int count = 0;
+
             for (int x = startValue; x <= stopValue; x++)
             {
-                if (Math.Cos(x) + 1 == 0)
+                double y;
+
+                
+                double denominator = Math.Cos(x) + 1;
+                if (Math.Abs(denominator) < 1e-12)
                 {
-                    valueArray[count] = 0;
+                    y = 0;
                 }
                 else
                 {
-                    y = 3 * x + 2 - x / (Math.Cos(x) + 1);
-                    valueArray[count] = y;
-                }
-                count++;
+                    
+                    y = 3 * x + 2 - x / denominator;
 
-            }
+                    y = Math.Round(y, 2, MidpointRounding.AwayFromZero);
+                }
+
+                valueArray[count] = y;
+                count++;
+            
+        }
             return valueArray;
         }
     }
--- Tyuiu.BudykovaMM.Sprint6.Task2.V7.Test/DataServiceTest.cs
+++ Tyuiu.BudykovaMM.Sprint6.Task2.V7.Test/DataServiceTest.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.IO;
 using Tyuiu.BudykovaMM.Sprint6.Task2.V7.Lib;
-using System;
 
 namespace Tyuiu.BudykovaMM.Sprint6.Task2.V7.Test
 {
@@ -14,27 +13,25 @@ namespace Tyuiu.BudykovaMM.Sprint6.Task2.V7.Test
 
             int startValue = -5;
             int stopValue = 5;
-
             int len = stopValue - startValue + 1;
 
-            
+            // Ожидаемые значения (уже округлённые до 2 знаков)
             double[] valueWaitArray = new double[len];
-            valueWaitArray[0] = -9.10;   // x = -5
-            valueWaitArray[1] = 1.55;    // x = -4
-            valueWaitArray[2] = 292.78;  // x = -3
-            valueWaitArray[3] = -0.57;   // x = -2
-            valueWaitArray[4] = -0.35;   // x = -1
-            valueWaitArray[5] = 2.00;    // x = 0
-            valueWaitArray[6] = 4.35;    // x = 1
-            valueWaitArray[7] = 4.57;    // x = 2
-            valueWaitArray[8] = -288.78; // x = 3
-            valueWaitArray[9] = 2.45;    // x = 4
-            valueWaitArray[10] = 13.10;  // x = 5
+            valueWaitArray[0] = -9.1;     // -9.10
+            valueWaitArray[1] = 1.55;     // 1.55
+            valueWaitArray[2] = 292.78;   // 292.78
+            valueWaitArray[3] = -0.57;    // -0.57
+            valueWaitArray[4] = -0.35;    // -0.35
+            valueWaitArray[5] = 2.0;      // 2.00
+            valueWaitArray[6] = 4.35;     // 4.35
+            valueWaitArray[7] = 4.57;     // 4.57
+            valueWaitArray[8] = -288.78;  // -288.78
+            valueWaitArray[9] = 2.45;     // 2.45
+            valueWaitArray[10] = 13.1;    // 13.10
 
+            double[] res = ds.GetMassFunction(startValue, stopValue);
 
-            double[] res;
-            res = new double[len];
-            res = ds.GetMassFunction(startValue, stopValue);
+            // Проверяем точное совпадение (после округления в методе)
             CollectionAssert.AreEqual(valueWaitArray, res);
         }
     }
